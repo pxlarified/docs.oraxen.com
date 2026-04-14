d1f16ca3bf0c14cda248b20bf0d3132fe34a8c59
pxlarified <jan.kramer@mizius.com>
2026-04-14 08:25:33 +0200

    update fine details on sounds and appearance + add highlight component to default

--- mdx-components.ts
+++ mdx-components.ts
@@ -1,10 +1,12 @@
 import { useMDXComponents as getDocsMDXComponents } from "nextra-theme-docs";
 import PluginFileTree from "./components/PluginFileTree";
+import Highlight from "./components/Highlight";
 
 const docsComponents = getDocsMDXComponents();
 
 export const useMDXComponents = (components?: Record<string, any>) => ({
   ...docsComponents,
   PluginFileTree,
+  Highlight,
   ...(components || {}),
 });
